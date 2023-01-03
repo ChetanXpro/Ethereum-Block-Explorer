aa1d97cea152290f69cdb88e1f639db64ce51f31
ChetanXpro <chetanbaliyan10@gmail.com>
2023-01-04 02:50:40 +0530

    Added block table

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,17 +8,25 @@ import {
   bytesToHex,
   bytesToUtf8,
 } from "ethereum-cryptography/utils";
+import BlockTable from "../BlockTable/BlockTable";
+import { textTransforms } from "@nextui-org/react";
 // import { isBytes } from "alchemy-sdk/dist/src/api/utils";
 
-// const { utf8ToBytes, toHex } = require("ethereum-cryptography/utils");
+import { checkResultErrors } from "@ethersproject/abi";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const settings = {
   apiKey: import.meta.env.VITE_ALCHEMY_API,
   network: Network.ETH_MAINNET,
 };
 
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
 const Home = () => {
@@ -29,32 +37,95 @@ const Home = () => {
     return parseInt(hexString, 16);
   }
 
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    {
+      key: 0,
+      block: 3232323,
+      miner: "0x023232232",
+    },
+  ];
+  async function getBlock() {
+    let data = [];
+    // console.log(data);
+
+    const blockHeight = await alchemy.core.getBlockNumber();
+
+    for (let i = blockHeight; i >= blockHeight - 10; i--) {
+      const block = await alchemy.core.getBlock(i);
+      const hr = new Date(block.timestamp).getHours();
+      const min = new Date(block.timestamp).getMinutes();
+      const age = `${hr}h:${min}min`;
+
+      let obj = {
+        key: block.number,
+        block: block.number,
+        miner: block.miner,
+        txn: block.transactions.length,
+        age: age,
+      };
+      data.push(obj);
+
+      // if(data.length > 2){
+      const check = blocks.filter((bk) => bk.key === block.number);
+      // console.log(check.length === 0);
+      if (check.length === 0) {
+        setBlock(() => {
+          return [...blocks, ...data];
+        });
+      }
+      // }
+    }
+  }
+
   useEffect(() => {
     async function getBlockNumber() {
       setBlockNumber(await alchemy.core.getBlockNumber());
     }
-    async function getBlock() {
-      const block = await alchemy.core.getBlock(16327151);
-      const data = block || [];
-
-      // var yourNumber = parseInt(newda, 16);
-      // console.log(yourNumber);
-
-      console.log(hexToDec(block._difficulty._hex));
-
-      // setBlock(data.transactions);
-    }
 
     getBlockNumber();
     getBlock();
-  });
+  }, []);
 
   return (
     <div className={`h-screen ${classes.main}`}>
-      <div className="h-34">{blockNumber}</div>
-      {/* {blocks.map((txn) => (
-        <div key={txn.from}>{JSON.stringify(txn.from)}</div>
-      ))} */}
+      {/* <div className="h-34">{blockNumber}</div> */}
+      <TableContainer className="bg-green-500" component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Block</TableCell>
+              {/* <TableCell>Age</TableCell> */}
+              <TableCell align="right">Txn</TableCell>
+              <TableCell align="right">Age</TableCell>
+              <TableCell align="right">Miner</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {blocks.map((row) => (
+              <TableRow
+                key={row.key}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell
+                  onClick={() => console.log(row.block)}
+                  component="th"
+                  scope="row"
+                >
+                  {row.block}
+                </TableCell>
+                {/* <TableCell align="right">{row.age}</TableCell> */}
+                <TableCell align="right">{row.txn}</TableCell>
+                <TableCell align="right">{row.age}</TableCell>
+                <TableCell align="right">{row.miner}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
